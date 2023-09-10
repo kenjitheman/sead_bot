e07ff796da424d3e3166e6fb37f156fd96b81392
kenjitheman <kenjitheman@proton.me>
2023-09-10 10:28:44 +0300

    feat: http server

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"net/http"
+	"os"
+
 	"github.com/kenjitheman/seadclub_bot/tg"
 )
 
 func main() {
-	tg.Start()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	})
+
+	go tg.Start()
+
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		panic(err)
+	}
 }
