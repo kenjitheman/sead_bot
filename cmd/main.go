886600aff2e5871a482a5d3ee390473f521926ed
kenjitheman <kenjitheman@proton.me>
2023-10-03 11:31:32 +0300

    feat: main.go, server deleted

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,9 @@
 package main
 
 import (
-	"net/http"
-	"os"
-
 	"github.com/kenjitheman/seadclub_bot/tg"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	})
-
-	go tg.Start()
-
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		panic(err)
-	}
+	tg.Start()
 }
